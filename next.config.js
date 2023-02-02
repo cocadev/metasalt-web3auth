16ec66465ac8afaf30e0dadc0fac5b4f284125f6
cocadev <40970351+cocadev@users.noreply.github.com>
2023-02-02 04:47:47 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  swcMinify: false,
   images: {
     domains: ['metasalt.io', 'minedn.io', 'ipfs.io', 'ipfs.moralis.io', 'nftavatarmaker.com', 'lh3.googleusercontent.com']
   },
